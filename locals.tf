e097ca4c198e3e1d44fb15ddbe1d8d63aa3896b4
Sunny <77861511+someoneelsescloud@users.noreply.github.com>
2021-05-22 12:07:04 +1000

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -1,7 +1,6 @@
 ########### Locals ###########
 locals {
   name       = random_pet.name.id
-  locationid = random_shuffle.locations.result
   public_key = ""
   region     = "ap-southeast-2"
   region_az  = "ap-southeast-2a"
@@ -9,19 +8,4 @@ locals {
 
 resource "random_pet" "name" {
   length = 1
-}
-
-resource "random_shuffle" "locations" {
-  input        = ["eastus", "westus"]
-  result_count = 2
-}
-
-resource "random_password" "password" {
-  length           = 16
-  special          = true
-  override_special = "_%@"
-}
-
-resource "random_id" "prefix" {
-  byte_length = 8
 }
